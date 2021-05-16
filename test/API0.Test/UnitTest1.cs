ae3b2be2ce41e202b633ecb62ad345c9c6cb0b5c
cyrilbesnard <cyrilbesnard@soprasteria.com>
2021-05-16 18:00:53 +0200

    first commit

---
+++ test/API0.Test/UnitTest1.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+using API0.Controllers;
+
+namespace API0.Test
+{
+    public class UnitTest1
+    {
+        WeatherForecastController controller = new WeatherForecastController(null);
+        
+        [Fact] void GetReturn()
+        {
+            var returnValue = controller.Get(10).Value;
+            Assert.Equal("10", returnValue);
+        }
+
+        [Fact] void Test1()
+        {
+
+        }
+    }
+}
